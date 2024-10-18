0903106d5592ba0584accfaf91a5f2a6e3d89d68
Pascal Brand <pascal.brand38@gmail.com>
2024-10-18 19:19:07 +0200

    Retrieve width / height of each image
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- index.mjs
+++ index.mjs
@@ -1,29 +1,39 @@
 import fs from 'fs'
 import path from 'path'
-import {joinImages} from 'join-images'
+import { joinImages } from 'join-images'
+import sharp from 'sharp'
 
 const config = {
   srcDir: 'data',
-  inputExtension: [ '.png' ],
-  outputSprites: [ 'tmp/out.png', 'tmp/out.webp' ],
+  inputExtension: ['.png'],
+  outputSprites: ['tmp/out.png', 'tmp/out.webp'],
 
 }
 
 let srcFiles = []
-fs.readdirSync(config.srcDir).forEach(file => {
+await Promise.all(fs.readdirSync(config.srcDir).map(async (file) => {
   const fullName = path.join(config.srcDir, file);
   if (fs.statSync(fullName).isFile()) {
     // it is a file. Checks it ends with inputExtension
     if (config.inputExtension.some(ext => file.endsWith(ext))) {
-      srcFiles.push(fullName)
+      const image = sharp(fullName);
+      const metadata = await image.metadata()
+      srcFiles.push( { fullName, width: metadata.width, height: metadata.height, } )
     }
   }
-});
+}))
 
+let positions = []
+srcFiles.forEach(src => {
+  positions.push({ src: src.fullName, offsetX: 0, offsetY: 0 })
+})
 
-joinImages(srcFiles).then((img) => {
+joinImages(positions).then((img) => {
   // Save image as file
   config.outputSprites.forEach(output =>
     img.toFile(output)
   )
 });
+
+console.log(srcFiles)
+console.log('DONE')
