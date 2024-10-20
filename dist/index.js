4595de12efcc22063a0a729a64f83ca099e1d212
Pascal Brand <pascal.brand38@gmail.com>
2024-10-20 21:01:15 +0200

    TS: js to ts
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- dist/index.js
+++ dist/index.js
@@ -48,10 +48,13 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = require("fs");
-var path_1 = require("path");
-var sharp_1 = require("sharp");
+var fs_1 = __importDefault(require("fs"));
+var path_1 = __importDefault(require("path"));
+var sharp_1 = __importDefault(require("sharp"));
 var url_1 = require("url");
 var defaultConfig = {
     src: {
