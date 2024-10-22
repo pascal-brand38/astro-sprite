f2f03645a2df493805282f4c101d74c368197af0
Pascal Brand <pascal.brand38@gmail.com>
2024-10-22 16:28:51 +0200

    TS: final step
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- dist/index.js
+++ dist/index.js
@@ -1,114 +1,52 @@
-"use strict";
 // Copyright (c) Pascal Brand
 // MIT License
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
-    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = __importDefault(require("fs"));
-var path_1 = __importDefault(require("path"));
-var sharp_1 = __importDefault(require("sharp"));
-var url_1 = require("url");
-var defaultConfig = {
+import fs from 'fs';
+import path from 'path';
+import sharp from 'sharp';
+import { fileURLToPath } from 'url';
+const defaultConfig = {
     src: {
         dir: 'assets/astro-sprite',
+        dirDev: '',
         extensions: ['.png', 'webp'],
     },
     dst: {
         spriteFile: 'img/astro-sprite.webp',
+        spriteFileDev: '',
         cssMainClass: '.astro-sprite',
         cssFile: 'css/astro-sprite.css',
+        cssFileDev: '',
         cssPrefix: '.astro-sprite-',
         cssSelector: '',
     }
 };
-var spriteConfig;
+let spriteConfig;
 // read all icons data
-function getIcons(config) {
-    return __awaiter(this, void 0, void 0, function () {
-        var icons;
-        var _this = this;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    icons = [];
-                    return [4 /*yield*/, Promise.all(fs_1.default.readdirSync(config.src.dirDev).map(function (file) { return __awaiter(_this, void 0, void 0, function () {
-                            var fullName, image, metadata;
-                            return __generator(this, function (_a) {
-                                switch (_a.label) {
-                                    case 0:
-                                        fullName = path_1.default.join(config.src.dirDev, file);
-                                        if (!fs_1.default.statSync(fullName).isFile()) return [3 /*break*/, 2];
-                                        if (!config.src.extensions.some(function (ext) { return file.endsWith(ext); })) return [3 /*break*/, 2];
-                                        image = (0, sharp_1.default)(fullName);
-                                        return [4 /*yield*/, image.metadata()];
-                                    case 1:
-                                        metadata = _a.sent();
-                                        icons.push({ name: path_1.default.parse(file).name, fullName: fullName, width: metadata.width, height: metadata.height, });
-                                        _a.label = 2;
-                                    case 2: return [2 /*return*/];
-                                }
-                            });
-                        }); }))];
-                case 1:
-                    _a.sent();
-                    return [2 /*return*/, icons];
+async function getIcons(config) {
+    let icons = [];
+    await Promise.all(fs.readdirSync(config.src.dirDev).map(async (file) => {
+        const fullName = path.join(config.src.dirDev, file);
+        if (fs.statSync(fullName).isFile()) {
+            // it is a file. Checks it ends with inputExtension
+            if (config.src.extensions.some(ext => file.endsWith(ext))) {
+                const image = sharp(fullName);
+                const metadata = await image.metadata();
+                icons.push({
+                    name: path.parse(file).name,
+                    fullName,
+                    width: metadata.width ? metadata.width : 0,
+                    height: metadata.height ? metadata.height : 0,
+                });
             }
-        });
-    });
+        }
+    }));
+    return icons;
 }
 // get the positions of all icons in the sprite
 function getPositions(icons) {
-    var positions = [];
-    var left = 0;
-    icons.forEach(function (icon) {
+    let positions = [];
+    let left = 0;
+    icons.forEach(icon => {
         // input, left and top are used by further call to sharp.composite()
         // do not change these property name
         positions.push({ input: icon.fullName, name: icon.name, left: left, top: 0, height: icon.height, width: icon.width });
@@ -117,66 +55,54 @@ function getPositions(icons) {
     return positions;
 }
 function createDirOfFile(fullName) {
-    var dirname = path_1.default.dirname(fullName);
-    if (!fs_1.default.existsSync(dirname)) {
-        fs_1.default.mkdirSync(dirname, { recursive: true });
+    const dirname = path.dirname(fullName);
+    if (!fs.existsSync(dirname)) {
+        fs.mkdirSync(dirname, { recursive: true });
     }
 }
 // write the resulting css
 function writeCss(positions, config) {
-    var cssText = "/* Auto-generated by astro-sprite */\n";
-    cssText += "".concat(config.dst.cssMainClass, " { background-image:url(/").concat(config.dst.spriteFile, ");}\n");
-    positions.forEach(function (position) {
-        cssText += "".concat(config.dst.cssPrefix).concat(position.name).concat(config.dst.cssSelector, " { background-position: -").concat(position.left, "px -").concat(position.top, "px; width: ").concat(position.width, "px; height: ").concat(position.height, "px; }\n");
+    let cssText = `/* Auto-generated by astro-sprite */\n`;
+    cssText += `${config.dst.cssMainClass} { background-image:url(/${config.dst.spriteFile});}\n`;
+    positions.forEach(position => {
+        cssText += `${config.dst.cssPrefix}${position.name}${config.dst.cssSelector} { background-position: -${position.left}px -${position.top}px; width: ${position.width}px; height: ${position.height}px; }\n`;
     });
     createDirOfFile(config.dst.cssFileDev);
-    fs_1.default.writeFileSync(config.dst.cssFileDev, cssText);
+    fs.writeFileSync(config.dst.cssFileDev, cssText);
 }
 function writeSprite(positions, config) {
-    var sprite = (0, sharp_1.default)({
+    const sprite = sharp({
         create: {
             background: { alpha: 0, b: 0, g: 0, r: 0 },
             channels: 4,
-            height: Math.max.apply(Math, positions.map(function (position) { return position.top + position.height; })),
-            width: Math.max.apply(Math, positions.map(function (position) { return position.left + position.width; })),
+            height: Math.max(...positions.map(position => position.top + position.height)),
+            width: Math.max(...positions.map(position => position.left + position.width)),
         },
     });
     sprite.composite(positions);
     createDirOfFile(config.dst.spriteFileDev);
     sprite.toFile(config.dst.spriteFileDev);
 }
-function runAstroSprite(config) {
-    return __awaiter(this, void 0, void 0, function () {
-        var icons, positions;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, getIcons(config)];
-                case 1:
-                    icons = _a.sent();
-                    positions = getPositions(icons);
-                    writeSprite(positions, config);
-                    writeCss(positions, config); // must write css after the sprite to know the sprite sha1
-                    return [2 /*return*/];
-            }
-        });
-    });
+async function runAstroSprite(config) {
+    const icons = await getIcons(config);
+    const positions = getPositions(icons);
+    writeSprite(positions, config);
+    writeCss(positions, config); // must write css after the sprite to know the sprite sha1
 }
-var AstroConfig = {
+const AstroConfig = {
     name: 'astro-sprite',
     hooks: {
-        "astro:config:done": function (_a) {
-            var astroConfig = _a.config;
-            spriteConfig.src.dirDev = path_1.default.join((0, url_1.fileURLToPath)(astroConfig.srcDir), spriteConfig.src.dir);
-            spriteConfig.dst.spriteFileDev = path_1.default.join((0, url_1.fileURLToPath)(astroConfig.publicDir), spriteConfig.dst.spriteFile);
-            spriteConfig.dst.cssFileDev = path_1.default.join((0, url_1.fileURLToPath)(astroConfig.srcDir), spriteConfig.dst.cssFile);
+        "astro:config:done": function ({ config: astroConfig }) {
+            spriteConfig.src.dirDev = path.join(fileURLToPath(astroConfig.srcDir), spriteConfig.src.dir);
+            spriteConfig.dst.spriteFileDev = path.join(fileURLToPath(astroConfig.publicDir), spriteConfig.dst.spriteFile);
+            spriteConfig.dst.cssFileDev = path.join(fileURLToPath(astroConfig.srcDir), spriteConfig.dst.cssFile);
             runAstroSprite(spriteConfig);
         },
     },
 };
 // initialize the astro sprite integration
-function astroSprite(config) {
-    if (config === void 0) { config = {}; }
-    spriteConfig = __assign(__assign({}, defaultConfig), config);
+function astroSprite(config = {}) {
+    spriteConfig = { ...defaultConfig, ...config };
     return AstroConfig;
 }
-exports.default = astroSprite;
+export default astroSprite;
