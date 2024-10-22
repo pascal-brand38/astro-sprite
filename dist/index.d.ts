7779d2ef6a863329847ef2439ef9bd76d160195f
Pascal Brand <pascal.brand38@gmail.com>
2024-10-22 18:44:20 +0200

    rm dirDev, spriteFileDev and cssFileDev
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,15 +1,12 @@
 import type { AstroIntegration } from 'astro';
 interface srcType {
     dir: string;
-    dirDev: string;
     extension: string;
 }
 interface dstType {
     spriteFile: string;
-    spriteFileDev: string;
     cssMainClass: string;
     cssFile: string;
-    cssFileDev: string;
     cssPrefix: string;
     cssSelector: string;
 }
